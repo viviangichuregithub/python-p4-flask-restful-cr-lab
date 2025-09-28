c785fd943b0c91093670314e1018d3f3a7dbfddc
Vivian Gichure <viviangichure@gmail.com>
2025-09-28 16:36:18 +0300

    completed lab

--- server/models.py
+++ server/models.py
@@ -1,21 +1,15 @@
-# server/models.py
-
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model):
+class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    image = db.Column(db.String(200), nullable=True)
-    price = db.Column(db.Float, nullable=True)
+    name = db.Column(db.String)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "image": self.image,
-            "price": self.price,
-        }
+    def __repr__(self):
+        return f'<Plant {self.name} | Price: {self.price}>'
